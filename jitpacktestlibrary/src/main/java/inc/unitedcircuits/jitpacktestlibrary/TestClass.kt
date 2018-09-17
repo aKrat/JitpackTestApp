237b170526f6615298098fd46e8f4358f14eeb52
Alex Krat <>
2018-09-17 16:42:30 +0300

    Initial commit

---
+++ jitpacktestlibrary/src/main/java/inc/unitedcircuits/jitpacktestlibrary/TestClass.kt
@@ -0,0 +1,12 @@
+package inc.unitedcircuits.jitpacktestlibrary
+
+class TestClass {
+
+    fun add(x: Double, y: Double) = x + y
+
+    fun deduct(x: Double, y: Double) = x - y
+
+    fun multiply(x: Double, y:Double) = x * y
+
+    fun divide(x: Double, y: Double) = x / y
+}
